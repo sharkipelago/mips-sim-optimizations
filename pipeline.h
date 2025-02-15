30ed731be0cd79f359f8c26951bb46ff84c699ac
chengJazhua <chengjazhua@gmail.com>
2025-02-15 12:25:42 -0500

    added initializer to pipeline structs

--- pipeline.h
+++ pipeline.h
@@ -5,91 +5,91 @@
 #include <iostream>
 
 struct FetchDecodePipeReg {
-    uint32_t pc;
-    uint32_t instruction;
+    uint32_t pc = 0;
+    uint32_t instruction = 0;
 };
 
 struct DecodeExPipeReg {
     //Control
-    bool reg_dest_control; 
-    bool jump_control;
-    bool jump_reg_control;
-    bool link_control;
-    bool shift_control;
-    bool branch_control;
-    bool bne_control;
-    bool mem_read_control;
-    bool mem_to_reg_control;
-    bool ALU_op_control;
-    bool mem_write_control;
-    bool halfword_control;
-    bool byte_control;
-    bool ALU_src_control;
-    bool reg_write_control;
-    bool zero_extend_control;
+    bool reg_dest_control = 0; 
+    bool jump_control = 0;
+    bool jump_reg_control = 0;
+    bool link_control = 0;
+    bool shift_control = 0;
+    bool branch_control = 0;
+    bool bne_control = 0;
+    bool mem_read_control = 0;
+    bool mem_to_reg_control = 0;
+    bool ALU_op_control = 0;
+    bool mem_write_control = 0;
+    bool halfword_control = 0;
+    bool byte_control = 0;
+    bool ALU_src_control = 0;
+    bool reg_write_control = 0;
+    bool zero_extend_control = 0;
 
     //Main Reg
-    uint32_t read_data_1;
-    uint32_t read_data_2;
+    uint32_t read_data_1 = 0;
+    uint32_t read_data_2 = 0;
 
 
-    int opcode;
-    int addr;
-    int rt;
-    int rd;
-    int shamt;
-    int funct;
-    uint32_t imm;
-    uint32_t pc;
+    int opcode = 0;
+    int addr = 0;
+    int rt = 0;
+    int rd = 0;
+    int shamt = 0;
+    int funct = 0;
+    uint32_t imm = 0;
+    uint32_t pc = 0;
 };
 
 struct ExMemPipeReg {
     // bool reg_dest_control; 
     // bool jump_control;
     // bool jump_reg_control;
-    bool link_control;
+    bool link_control = 0;
     // bool shift_control;
-    bool branch_control;
-    bool bne_control;   
-    bool mem_read_control;
-    bool mem_to_reg_control;
+    bool branch_control = 0;
+    bool bne_control = 0;   
+    bool mem_read_control = 0;
+    bool mem_to_reg_control = 0;
     // bool ALU_op_control;
-    bool mem_write_control;
-    bool halfword_control;
-    bool byte_control;
+    bool mem_write_control = 0;
+    bool halfword_control = 0;
+    bool byte_control = 0;
     // bool ALU_src_control;
-    bool reg_write_control;
+    bool reg_write_control = 0;
     // bool zero_extend_control;
     
-    uint32_t pc_add_result;
+    uint32_t pc_add_result = 0;
 
-    uint32_t alu_zero ;
-    uint32_t alu_result;
-    uint32_t read_data_2;
-    uint32_t write_reg;
+    uint32_t alu_zero = 0;
+    uint32_t alu_result = 0;
+    uint32_t read_data_2 = 0;
+    uint32_t write_reg = 0;
 };
 
 struct MemWBPipeReg {
     // bool reg_dest_control; 
     // bool jump_control;
     // bool jump_reg_control;
-    bool link_control;
+    bool link_control = 0;
     // bool shift_control;
     // bool branch_control;
     // bool bne_control;
     // bool mem_read_control;
-    bool mem_to_reg_control;
+    bool mem_to_reg_control = 0;
     // bool ALU_op_control;
     // bool mem_write_control;
     // bool halfword_control;
     // bool byte_control;
     // bool ALU_src_control;
-    bool reg_write_control;
+    bool reg_write_control = 0;
     // bool zero_extend_control;
 
-    uint32_t read_data_mem;
-    uint32_t alu_result; 
-    int write_reg;
+    uint32_t read_data_mem = 0;
+    uint32_t alu_result = 0; 
+    int write_reg = 0;
 };  
 
 
